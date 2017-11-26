81d7dc52690bc1406f82afc14b1baa4fdabc8913
Michał Dziewanowski <michal.dziewanowski@coderion.pl>
2017-11-26 15:53:56 +0100

    Dodane materiały

--- app/src/main/java/com/example/coderion/app/MainActivity.java
+++ app/src/main/java/com/example/coderion/app/MainActivity.java
@@ -29,27 +29,6 @@ public class MainActivity extends AppCompatActivity {
 
         newBoard = findViewById(R.id.newBoard);
         stock = findViewById(R.id.stock);
-
-        String[] a = {"Tekstolit", "Zielone szkło", "Brązowe szkło", "Czerwone szkło", "Delmat Epoxy", "Czarne szkło", "Żółte szkło"};
-        String[] w = {"2000x1000", "2000x1500", "1000x1000", "2500x1500", "1000x500", "2000x2000", "1500x500"};
-        Double[] g = {5.0, 2.3, 13.0, 14.3, 10.0, 6.5, 7.0};
-        String[] p = {"23F", "2G", "13BA", "10A", "11A", "12B", "8G"};
-//        for (int i = 0; i < 7; ++i) {
-//            Board plyta = new Board();
-//            plyta.setMaterial(a[i]);
-//            plyta.setSize(w[i]);
-//            plyta.setThickness(g[i]);
-//            plyta.setPlace(p[i]);
-//
-//            byte[] array = null;
-//            HttpStatus httpStatus = null;
-//            try {
-//                httpStatus = new RestService(plyta, array, Values.ADD_NEW_BOARD).execute(Values.MAIN_LINK).get().getStatusCode();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//                Toast.makeText(this, "Nie udało się dodać płyty", Toast.LENGTH_SHORT);
-//            }
-//        }
     }
 
     public void addBoardPanel(View view) {
